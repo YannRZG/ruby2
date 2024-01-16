992058f5bd5ea2a1417e0e7420d40da0f0d4490e
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-01-16 23:29:17 +0100

    main

--- exo-09.rb
+++ exo-09.rb
@@ -1,7 +1,7 @@
 puts "Entre ton année de naissance :"
 print "> "
-year_of_birth = gets.chomp.to_i
-today = 2024
-for year in year_of_birth..today
-    puts year 
+year_of_birth = gets.chomp.to_i  # age actuel
+today = 2024 # aujourd'hui
+for year in year_of_birth..today # de la date de naissance à aujourd'hui 
+    puts year # donne moi les années
 end
---
+++ exo-10.rb
@@ -0,0 +1,12 @@
+puts "Entre ton age:"
+print "> "
+age = gets.chomp.to_i
+today = 2024
+
+for year in age..today
+    puts "il y a #{age} ans ; tu avais #{today - year} ans"
+
+##### solution pas trouver , temps écoulé :'(
+
+
+end
